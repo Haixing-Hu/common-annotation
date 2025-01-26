078644021b0329947ebe1e05b6d6be362a33f228
Haixing Hu <starfish.hu@gmail.com>
2025-01-26 23:59:05 +0800

    feat: add @AsciiText annotation

---
+++ src/main/java/ltd/qubit/commons/annotation/AsciiText.java
@@ -0,0 +1,28 @@
+/// /////////////////////////////////////////////////////////////////////////////
+//
+//    Copyright (c) 2024 - 2025.
+//    Nanjing Xinglin Digital Technology Co., Ltd.
+//
+//    All rights reserved.
+//
+////////////////////////////////////////////////////////////////////////////////
+package ltd.qubit.commons.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+/**
+ * 此标注指定一个字段的值必须是ASCII字符。
+ *
+ * @author 胡海星
+ */
+@Retention(RUNTIME)
+@Target({FIELD})
+@Documented
+public @interface AsciiText {
+  //  empty
+}
